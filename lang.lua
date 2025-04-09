9e8ba46b71a976296999ff707ab8a934b8ecd588
AitLilCat <69047868+AitLilCat-Script@users.noreply.github.com>
2025-04-09 23:41:12 +0200

    Update lang.lua
    Consistent Text Formatting:
    
    Made some phrases more consistent, like "No spawn locations found..." instead of "No spawn locations in config file..."
    
    Updated CarModelLoadingTimeout to be clearer ("please check if the model names are correct").
    
    String Concatenation:
    
    Improved readability of KeepTheCar and SubtractedXP by separating dynamic content and keeping the structure more readable.
    
    Dynamic Text Formatting:
    
    The text_GoFindCar and text_GoFindParkedCar functions now use string.format(), which is easier to read and manage than string concatenation. It makes the dynamic text generation cleaner, more maintainable, and less prone to errors.
    
    Clarified some phrases:
    
    Some phrases were adjusted for clarity, such as "Displays your truck delivery XP" and "XP to next truck delivery rank."

--- lang.lua
+++ lang.lua
@@ -1,13 +1,13 @@
 -- For other languages, translate these strings
 blipName = "Truck Delivery"
-NoSpawnLocationInConfigFile = "No spawn locations in config file, quitting the job"
+NoSpawnLocationInConfigFile = "No spawn locations found in the config file, quitting the job"
 KeepTheCar_JobIsCancelled = "Keep the truck, job is canceled"
 VehicleHasBeenDestroyed_JobIsCancelled = "The truck was destroyed! Job has been canceled."
 JobQuit = "Truck delivery job has been quit"
 JobStarted = "Truck delivery job started"
-StartJob = "Press E to start a job"
+StartJob = "Press E to start the job"
 QuitJob = "Press E to quit the job"
-CarModelLoadingTimeout = "Truck model loading has timed out, check if model names of your cars are correct"
+CarModelLoadingTimeout = "Truck model loading has timed out, please check if the model names are correct"
 text_adminXPEditDescription = "(Admin) Edit truck delivery rank"
 text_adminXPEditCommand = "cardeliveryxp"
 text_option = "Option"
@@ -19,24 +19,26 @@ text_number = "Number"
 text_level = "Level"
 text_levelUp = "Level up"
 text_levelLost = "Level lost"
-text_numberHelp = "Number of change"
-text_noSuchParameter = "No such parameter"
-text_notEveryArgumentWasEntered = "Not every argument was entered"
+text_numberHelp = "Number of changes"
+text_noSuchParameter = "No such parameter found"
+text_notEveryArgumentWasEntered = "Not all arguments were entered"
 text_getXpCommand = "deliveryxp"
-text_getXpDescription = "Shows your truck delivery xp"
+text_getXpDescription = "Displays your truck delivery XP"
 text_added = "Added"
-text_xpToCarDeliveryRank = "xp to truck delivery rank"
+text_xpToCarDeliveryRank = "XP to next truck delivery rank"
 text_cooldown = "Cooldown"
 text_secondsLeft = "seconds left"
 
-
+-- Dynamic Texts for Vehicle and Job Information
 KeepTheCar = "In the next " .. choiceTimer .. " seconds, press U to keep the truck and leave the job"
 SubtractedXP = tostring(rankPenalty) .. " XP subtracted from truck delivery rank"
 
+-- Function for finding a car
 function text_GoFindCar(spawns, level, vehicleChoice, spawnLocation)
-    return "Go find the ~g~" .. vehicles[level][vehicleChoice].name .. "~w~ somewhere around ~g~" .. spawns[spawnLocation].name .. "~w~!"
+    return string.format("Go find the ~g~%s~w~ somewhere around ~g~%s~w~!", vehicles[level][vehicleChoice].name, spawns[spawnLocation].name)
 end
 
+-- Function for finding a parked car
 function text_GoFindParkedCar(spawns, level, vehicleChoice, spawnLocation)
-    return "Go find the parked ~g~" .. vehicles[level][vehicleChoice].name .. "~w~ at ~g~" .. spawns[spawnLocation].name .. "~w~!"
+    return string.format("Go find the parked ~g~%s~w~ at ~g~%s~w~!", vehicles[level][vehicleChoice].name, spawns[spawnLocation].name)
 end
